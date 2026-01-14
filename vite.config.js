100f0342e8b4b6cf4b0f8fd3d995aa1f7eab1472
Jeet Madhvani <madhavanijeet123@gmail.com>
2026-01-14 16:46:12 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/expense-tracker/',
   plugins: [
     react(),tailwindcss()
   ],
+  base: '/expense-tracker'
 })
